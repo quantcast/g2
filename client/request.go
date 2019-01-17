ca5976e79e01fc3c573c76f59cd6f7da3545700b
Scott S. McCoy <tag@cpan.org>
2019-01-17 14:19:22 -0800

    client: gearman client now faster and better
    Eliminated the strange usage of in favor of using channels. Removed the
    redundant `processLoop` goroutine, the `readLoop` now inlines the
    processing (it was serial anyway!). Switched to the use of threadsafe
    memory object pools for request and response objects, and now allows
    pipelining if multiple goroutines are making requests at once.
    
    Removed a lot of redundancy in the code, and eliminated the use of the
    strange `responseHandlerMap` in favor of a concurrent map for holding
    callbacks on a per-handler basis.
    
    Refactored the `request` type such that it now uses a builder pattern,
    and now itself keeps a small byte buffer for each request which is
    reused when parameters are attached to requests. Streamlined the
    formatting of outbound messages such that they are encoded as they are
    written to the output buffer, to reduce the number of copies involved in
    most operations.
    
    Eliminated the strange blind-read loop which pretended it had no idea
    how large of a buffer it would need to allocate to read a response, and
    now allocates a buffer for each payload, the size of the payload as
    specified by the server, respectively.

--- client/request.go
+++ client/request.go
@@ -1,44 +1,40 @@
 package client
 
 import (
-	"encoding/binary"
-
 	rt "github.com/ssmccoy/g2/pkg/runtime"
 )
 
 // Request from client
 type request struct {
-	DataType rt.PT
-	Data     []byte
+	pt   rt.PT
+	data [][]byte
 }
 
-// Encode a Request to byte slice
-func (req *request) Encode() (data []byte) {
-	l := len(req.Data)           // length of data
-	tl := l + rt.MinPacketLength // add 12 bytes head
-	data = rt.NewBuffer(tl)
-	copy(data[:4], rt.ReqStr)
-	binary.BigEndian.PutUint32(data[4:8], req.DataType.Uint32())
-	binary.BigEndian.PutUint32(data[8:12], uint32(l))
-	copy(data[rt.MinPacketLength:], req.Data)
-	return
+func (req *request) args(args ...[]byte) {
+	req.data = req.data[:0]
+	req.data = append(req.data, args...)
 }
 
-func getRequest() (req *request) {
-	// TODO add a pool
-	req = &request{}
-	return
+func (req *request) submitJob(pt rt.PT, funcname, id string, arg []byte) *request {
+	req.pt = pt
+
+	req.args([]byte(funcname), []byte(id), arg)
+
+	return req
 }
 
-func getJob(id string, funcname, data []byte) (req *request) {
-	req = getRequest()
-	a := len(funcname)
-	b := len(id)
-	c := len(data)
-	l := a + b + c + 2
-	req.Data = rt.NewBuffer(l)
-	copy(req.Data[0:a], funcname)
-	copy(req.Data[a+1:a+b+1], []byte(id))
-	copy(req.Data[a+b+2:], data)
-	return
+func (req *request) status(handle string) *request {
+	req.pt = rt.PT_GetStatus
+
+	req.args([]byte(handle))
+
+	return req
+}
+
+func (req *request) echo(arg []byte) *request {
+	req.pt = rt.PT_EchoReq
+
+	req.args(arg)
+
+	return req
 }
